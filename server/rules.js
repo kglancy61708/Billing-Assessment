06323277250131493f38d6e2f89d66ae2ed03cc6
kglancy61708 <kglancy61708@gmail.com>
2026-07-29 13:23:55 -0500

    Revert BUILTIN.DF - use raw custentity318 value to restore scan stability

--- server/rules.js
+++ server/rules.js
@@ -33,8 +33,7 @@ async function rule1_missingOnlineInvoiceVsSiblings() {
   const siblingsByParent = {};
   if (parentIds.length > 0) {
     const siblingRows = await suiteQLAll(`
-      SELECT s.id, s.parent, s.companyname, s.custentity318,
-             BUILTIN.DF(s.custentity318) AS custentity318name
+      SELECT s.id, s.parent, s.companyname, s.custentity318
       FROM customer s
       WHERE s.parent IN (${parentIds.join(',')})
         AND s.isinactive = 'F'
@@ -47,7 +46,7 @@ async function rule1_missingOnlineInvoiceVsSiblings() {
       siblingsByParent[pid].push({
         id: String(s.id),
         companyname: s.companyname,
-        custentity318: s.custentity318name || s.custentity318 || null,
+        custentity318: s.custentity318 || null,
       });
     }
   }
